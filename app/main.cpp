f9719c2d82f3b76fedbc0070ce8ea7e41806e753
maurice <mauriceAlthoff@users.noreply.github.com>
2022-03-22 22:57:49 +0100

    fix structure

--- app/main.cpp
+++ app/main.cpp
@@ -4,14 +4,10 @@
 #include <filesystem>
 #include <vector>
 #include <memory>
+#include <regex>
 
 using namespace boost::program_options;
 
-/**
- * The Visitor Interface declares a set of visiting methods that correspond to
- * component classes. The signature of a visiting method allows the visitor to
- * identify the exact class of the component that it's dealing with.
- */
 class Smiley_Component;
 class Top_Ten_Component;
 
@@ -20,11 +16,6 @@ class Visitor {
   virtual void output_start_pos_smileys(const std::shared_ptr<Smiley_Component> element) const = 0;
   virtual void output_top_ten_words(const std::shared_ptr<Top_Ten_Component> element) const = 0;
 };
-
-/**
- * The Component interface declares an `accept` method that should take the base
- * visitor interface as an argument.
- */
 
 class Component {
  public:
@@ -36,18 +27,29 @@ protected:
   std::string m_text;  
 };
 
-/**
- * Each Concrete Component must implement the `Accept` method in such a way that
- * it calls the visitor's method corresponding to the component's class.
- */
 class Smiley_Component : public Component {
  public:
   Smiley_Component(const std::string& text):Component(text){}
   void compute(std::shared_ptr<Visitor> visitor) const override {
     visitor->output_start_pos_smileys(std::make_shared<Smiley_Component> (m_text));
   }
-  std::string compute_start_pos_smileys() const {
-    return "smileys " + m_text;
+  
+  std::optional<std::vector<int>> compute_start_pos_smileys() const {
+    std::regex rx("[:][-]?[\\/\\[\\]{}\\(\\)]");    
+    std::vector<int> index_matches;
+
+    for(auto it = std::sregex_iterator(m_text.begin(), m_text.end(), rx);
+	it != std::sregex_iterator();
+	++it)
+      {
+	index_matches.push_back(it->position());
+	std::cout << " " << std::to_string(it->position());
+      }
+    std::cout << "\n";
+
+    if(index_matches.empty())
+      return  std::nullopt;
+    return std::optional<std::vector<int>>(index_matches);
   }
 };
 
@@ -65,7 +67,9 @@ class Top_Ten_Component : public Component {
 class StandarOutput : public Visitor {
  public:
   void output_start_pos_smileys(const std::shared_ptr<Smiley_Component> element) const override {
-    std::cout << element->compute_start_pos_smileys() << " + Standard Output\n";
+    auto result = element->compute_start_pos_smileys();
+    if(result.has_value())
+      std::cout << result->size()  << " + Standard Output\n";
   }
 
   void output_top_ten_words(const std::shared_ptr<Top_Ten_Component> element) const override {
@@ -76,7 +80,9 @@ class StandarOutput : public Visitor {
 class SimpleOutput : public Visitor {
  public:
   void output_start_pos_smileys(const std::shared_ptr<Smiley_Component> element) const override {
-    std::cout << element->compute_start_pos_smileys() << " + Simple Output\n";
+    auto result = element->compute_start_pos_smileys();
+    if(result.has_value())
+      std::cout << result->size()  << " + Simple Output\n";
   }
   void output_top_ten_words(const std::shared_ptr<Top_Ten_Component> element) const override {
     std::cout << element->compute_top_ten_words() << " + Simple Output\n";
@@ -86,7 +92,9 @@ class SimpleOutput : public Visitor {
 class XmlOutput : public Visitor {
  public:
   void output_start_pos_smileys(const std::shared_ptr<Smiley_Component> element) const override {
-    std::cout << element->compute_start_pos_smileys() << " + XML Output\n";
+    auto result = element->compute_start_pos_smileys();
+    if(result.has_value())
+      std::cout << result->size()  << " + XML Output\n";
   }
   void output_top_ten_words(const std::shared_ptr<Top_Ten_Component> element) const override {
     std::cout << element->compute_top_ten_words() << " + XML Output\n";
@@ -107,7 +115,7 @@ public:
 
     std::cout << "If: " << m_console_output << "," << m_simple_output << "," << m_xml_output<<'\n';
   }
-private:
+  //private:
   bool m_console_output{false};
   bool m_simple_output{false};
   bool m_xml_output{false};
@@ -116,11 +124,6 @@ private:
   std::filesystem::path m_path_xml;
 };
 
-/**
- * The client code can run visitor operations over any set of elements without
- * figuring out their concrete classes. The accept operation directs a call to
- * the appropriate operation in the visitor object.
- */
 void ClientCode(const std::vector<std::shared_ptr<Component>>& components,
 		std::shared_ptr<Visitor> visitor) {
   std::for_each (components.begin(),
@@ -129,55 +132,55 @@ void ClientCode(const std::vector<std::shared_ptr<Component>>& components,
 }
 
 int main(int argc, const char* argv[])
-{
-  std::vector<std::shared_ptr<Component>> components =
-    {std::make_shared<Smiley_Component>("hello World"),
-     std::make_shared<Top_Ten_Component>("hello Top Ten")};
-  
-  std::cout << "The client code works with all visitors via the base Visitor interface:\n";
-  auto visitor1 = std::make_shared<StandarOutput>();
-  ClientCode(components, visitor1);
-  std::cout << "\n";
-  std::cout << "It allows the same client code to work with different types of visitors:\n";
-  auto visitor2 = std::make_shared<SimpleOutput>();
-  ClientCode(components, visitor2);
-  std::cout << "\n";
-  std::cout << "It allows the same client code to work with different types of visitors:\n";
-  auto visitor3 = std::make_shared<XmlOutput>();
-  ClientCode(components, visitor3);
-  
-    try {
-      //step 0 parse arguments
-        options_description desc { "Options" };
-        desc.add_options()
-	  ("help,h", "Help screen")
-	  ("console", value<bool>()->default_value(false), "console output")
-	  ("simple", value<std::string>()->default_value(""), "simple text output")
-	  ("xml", value<std::string>()->default_value(""), "xml file Output");
+{  
+  try {
+    //step 0 parse arguments
+    options_description desc { "Options" };
+    desc.add_options()
+      ("help,h", "Help screen")
+      ("console", value<bool>()->default_value(false), "console output")
+      ("simple", value<std::string>()->default_value(""), "simple text output")
+      ("xml", value<std::string>()->default_value(""), "xml file Output");
 
-        variables_map vm;
-        store(parse_command_line(argc, argv, desc), vm);
+    variables_map vm;
+    store(parse_command_line(argc, argv, desc), vm);
 
-        if (vm.count("help"))
-            std::cout << desc << '\n';
-        if (vm.count("console"))
-            std::cout << "console: " << vm["console"].as<bool>() << '\n';
-        if (vm.count("simple"))
-	  std::cout << "simple: " << vm["simple"].as<std::string>() << '\n';
-        if (vm.count("xml"))
-	  std::cout << "xml: " << vm["xml"].as<std::string>() << '\n';
-	//step 1 read input
+    if (vm.count("help"))
+      std::cout << desc << '\n';
+    if (vm.count("console"))
+      std::cout << "console: " << vm["console"].as<bool>() << '\n';
+    if (vm.count("simple"))
+      std::cout << "simple: " << vm["simple"].as<std::string>() << '\n';
+    if (vm.count("xml"))
+      std::cout << "xml: " << vm["xml"].as<std::string>() << '\n';
+    //step 1 read input
+    // skipped to test faster
 
-	//step 2 apply algorithm
+    //step 2 apply algorithm
+    std::vector<std::shared_ptr<Component>> components =
+      {std::make_shared<Smiley_Component>("hello:-] World:-]"),
+       std::make_shared<Top_Ten_Component>("hello :-] Top :-] Ten")};
 
-	//step 3 generate output
+    Input_filter filter(vm["console"].as<bool>(),
+			std::filesystem::path(vm["simple"].as<std::string>()),
+			std::filesystem::path(vm["xml"].as<std::string>()));
 
-	Input_filter filter(vm["console"].as<bool>(),
-			    std::filesystem::path(vm["simple"].as<std::string>()),
-			    std::filesystem::path(vm["xml"].as<std::string>()));
-    } catch (const error& ex) {
-        std::cerr << ex.what() << '\n';
+    //step 3 generate output
+    if (filter.m_console_output){
+      auto visitor1 = std::make_shared<StandarOutput>();
+      ClientCode(components, visitor1);
     }
+    if (filter.m_simple_output){
+      auto visitor2 = std::make_shared<SimpleOutput>();
+      ClientCode(components, visitor2);
+    }
+    if (filter.m_xml_output){
+      auto visitor3 = std::make_shared<XmlOutput>();
+      ClientCode(components, visitor3);
+    }
+  } catch (const error& ex) {
+    std::cerr << ex.what() << '\n';
+  }
     
-    return 0;
+  return 0;
 }
